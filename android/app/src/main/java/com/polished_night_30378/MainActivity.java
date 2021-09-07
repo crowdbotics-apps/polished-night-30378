319cf72fb691fe0a1e6becc499a2626ff1f27322
Crowdbotics <team@crowdbotics.com>
2021-09-07 23:37:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/polished_night_30378/MainActivity.java
@@ -0,0 +1,15 @@
+package com.polished_night_30378;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "polished_night_30378";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "polished_night_30378Identifier",
+  url: "https://polished_night_30378Identifier.botics.co",
+  api: "https://polished_night_30378Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
